4663c36ad76c0692002804c9fb88beb2bedf85fa
Lee-Martin Clarke <mobeel5356@gmail.com>
2017-11-14 14:43:18 -0500

    Aesthetics and Search funtion
    made stuff look a bit better and added basic search funtion

--- header.php
+++ header.php
@@ -22,7 +22,36 @@ session_start();
     </head>
     
     <div>
-        <input class="rightsearch">
+        
+        
+        <form>
+        <input type="text" size="30" onkeyup="showResult(this.value)" class="rightsearch">
+            <div id="livesearch"></div>
+        </form>
+            
+        <script>showResult(this.value)
+function showResult(str) {
+  if (str.length==0) { 
+    document.getElementById("livesearch").innerHTML="";
+    document.getElementById("livesearch").style.border="0px";
+    return;
+  }
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("livesearch").innerHTML=this.responseText;
+      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    }
+  }
+  xmlhttp.open("GET","livesearch.php?q="+str,true);
+  xmlhttp.send();
+}
+</script>
         
             <?php
             if($_SESSION['signed_in'])
@@ -55,5 +84,6 @@ session_start();
         </div>
     
     <body>
+
         
             <div class="container">
--- home.php
+++ home.php
@@ -6,14 +6,28 @@ include 'header.php';
 
 
 <h1>Computer Science</h1>
-        <div class="raoface">
+        <div class="leftfrontpage">
             <img src="https://grid.cs.gsu.edu/~ncasturi1/125.JPG" alt="Rao Face"/>
             <h5>Research Interests:</h5>
                 <p>My area of interests are in Database systems, Design, Modeling and Distributed Database computations along with Software Engineering. I also work on research topics which analyze in efficient and cost effective Data Warehouse frameworks and Data Mining.</p>
+            
             <h6>Publications:</h6>
                 <p>Rao Casturi, "Effective Aggregation - Precursor for Enterprise Data Mining Frameworks." "International Journal of Emerging Technology & Advanced Engineering (ISSN 2250-2459, ISO 9001:2008 Certified Journal), Volume 5, Issue 12, December, 2015. Pages:16-24</p>
                 <p>Rao Casturi, "Design and Development of an Efficient Calculation Framework for Internal Rate of Return (IRR) of a Fixed Income Portfolio with SIMD Architecture." "International Journal of Emerging Technology & Advanced Engineering (ISSN 2250-2459, ISO 9001:2008 Certified Journal), Volume 4, Issue 9, September, 2014. Pages:17-22</p>
         </div>
+<div class="rightfrontpage">
+<h2 style="color: darkblue;">Quick Links</h2>
+<br>
+<button class="button" style="display: inline-block;" >foobar Link 1</button>    
+<button class="button" style="display: inline-block;" >foobar Link 2</button> 
+<button class="button" style="display: inline-block;" >foobar Link 3</button> 
+<button class="button" style="display: inline-block;" >foobar Link 4</button> 
+<button class="button" style="display: inline-block;" >foobar Link 5</button> 
+<button class="button" style="display: inline-block;" >foobar Link 6</button> 
+
+</div>
+
+
 
 
 
@@ -33,8 +47,8 @@ function toggleEdit() {
 
 <div class="news">
     
-            <h2 style="color:black;">News Announcement</h2>
-    <div style="display: inline-block;">
+            <h2 style="color:black;">News & Announcement</h2>
+    <div>
             <div>
                 <?php  
 
@@ -51,31 +65,66 @@ function toggleEdit() {
                     while($row = mysqli_fetch_assoc($result))
                         {
                             echo '<br><h2 style="color:black; text-align:left;">' . $row['news_title'] . '</h2><br>';
-                            echo $row['news_content'] . '<br>';
+                            echo $row['news_content'];
+                            //echo $row['news_id'];    
+                        
+                        ?> 
+                <form method="post" action="">
+                <input class="button3" type="submit" style="display: inline-block;" name="delete<?php echo $row['news_id'] ?>" value="Delete this news" />
+                </form>
+                <?php
+                            $deletename = 'delete' . $row['news_id'];
+                       // echo $deletename;
+                            if(isset($_POST[$deletename])){
+                                
+                                $prep = "DELETE FROM news WHERE news_id='" . $row['news_id'] . "'";
+                                $command = mysqli_query($conn, $prep);
+                                
+                                if(!$command)
+                                    {
+                                        echo 'Your news has not been deleted, please try again later.'. mysqli_error($conn);
+                                    }
+                                    else
+                                    {
+                                         echo "<meta http-equiv='refresh' content='0'>";
+                                    } 
+                                
+                            }
+                        echo '<p></p>';
+                        echo '<p></p>';
+                        echo '<hr>';
+                        
                         }
 
                 }
+                
+               // if($_SESSION['user_level']==1){
 
                 ?> 
-            </div>
+            
     
     
             
-
-            <button class="button3" style="float: right; display: inline-block;" onclick="toggleEdit()">Add new Event</button>
+            <div style="text-align:center;">
+            <button class="button3" style="float: right; display: block;" onclick="toggleEdit()">Add new Event</button>
             
             <div class="news_input" style="display: none;" id="myDIV">
                 <!-- create event -->
-                    <form method="post" action="#">
-                    Title:<br><textarea name="news-title" style="max-height: 30px; width: 100%;"></textarea><br>
+                    <form method="post" action="">
+                    Title:<br><input class="text-input" type="text" name="news-title" style="max-height: 30px; width: 100%;"><br>
                     Announement:<br><textarea name="news-content" style="max-height: 100px; width: 100%;"></textarea>
-                    <input class="button3" type="submit" name="submit" value="Update Post" />
+                    <input class="button3" type="submit" name="submit" value="Submit" />
+                        
                     </form>  
                    
                 </div>
+        </div>
     </div>
+        </div>
     
             <?php  
+    
+              //  }
         
         
         
@@ -91,7 +140,7 @@ function toggleEdit() {
         }
         else
         {
-             header("Refresh:0");
+             echo "<meta http-equiv='refresh' content='0'>";
         } 
             
         }
